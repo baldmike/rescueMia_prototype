7802875c8594eaff17c59256326b63e5fabc4b07
Bald Mike <baldmike312@gmail.com>
2017-11-11 14:00:24 -0600

    added adopted option

--- apps/semi_restful_app/urls.py
+++ apps/semi_restful_app/urls.py
@@ -4,6 +4,11 @@ from views import *
 urlpatterns = [
     url(r'^$', index),
     url(r'^new_dog$', new_dog),
-    url(r'^select/(?P<dog_id>\d+)$', select)
+    url(r'^select/(?P<dog_id>\d+)$', select),
+    url(r'^adopted/(?P<dog_id>\d+)$', adopted),
+    url(r'^available/(?P<dog_id>\d+)$', available),
+    url(r'^select_adopted/(?P<dog_id>\d+)$', select_adopted),
+    url(r'^adopted_dogs$', adopted_dogs),
+
 ]
 
--- apps/semi_restful_app/views.py
+++ apps/semi_restful_app/views.py
@@ -21,15 +21,51 @@ def new_dog(request):
         fixed = request.POST['fixed']
         vaccine = request.POST['vaccine']
         adopted = False
-        Dog.objects.create(name=name, age=age, color=color, size=size, fixed=fixed, vaccine=vaccine)
+        Dog.objects.create(name=name, age=age, color=color, size=size, fixed=fixed, vaccine=vaccine, adopted=adopted)
     return redirect('/')
 
 def select(request, dog_id):
     current_dog = Dog.objects.filter(id=dog_id)
+    avail_dogs = Dog.objects.exclude(adopted=True)
     print current_dog
     context = {
-        "avail_dogs" : Dog.objects.exclude(adopted=True),
+        "avail_dogs" : avail_dogs,
         'current_dog' : current_dog,
 
     }
     return render(request, 'semi_restful_app/index.html', context)
+
+def select_adopted(request, dog_id):
+    current_dog = Dog.objects.filter(id=dog_id)
+    adopted_dogs = Dog.objects.filter(adopted=True)
+    print current_dog
+    context = {
+        "adopted_dogs" : adopted_dogs,
+        'current_dog' : current_dog,
+
+    }
+    return render(request, 'semi_restful_app/adopted.html', context)
+
+def adopted(request, dog_id):
+    adopted_dog = Dog.objects.get(id=dog_id)
+    adopted_dog.adopted = True
+    adopted_dog.save()
+
+    return redirect('/')
+
+def adopted_dogs(request):
+    adopted_dogs = Dog.objects.filter(adopted=True)
+    context = {
+
+        'adopted_dogs' : adopted_dogs
+
+    }
+
+    return render(request, 'semi_restful_app/adopted.html', context)
+
+def available(request, dog_id):
+    available_dog = Dog.objects.get(id=dog_id)
+    available_dog.adopted = False
+    available_dog.save()
+
+    return redirect('/adopted_dogs')
